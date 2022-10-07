8f42407acb9589064c0cee38a9a98689dd551942
Danilo Filitto <danilo.filitto@gmail.com>
2022-10-07 15:15:59 -0300

    Add files via upload

---
+++ Scripts/InterstitialAdMob.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class InterstitialAdMob : MonoBehaviour
+{
+    private InterstitialAd interstitial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        MobileAds.Initialize(initStatus => { });
+        RequestInterstitial();
+
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
+    }
+
+    private void RequestInterstitial()
+    {
+        #if UNITY_ANDROID
+                string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        #elif UNITY_IPHONE
+                string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+        #else
+                string adUnitId = "unexpected_platform";
+        #endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+}
